f55a74605876ae2c5c1d79842b7ddaa482633f71
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2022-12-18 20:32:51 -0600

    Additional styles

--- capture/src/components/FaqSection.js
+++ capture/src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+import { About } from '../styles';
 
 const FaqSection = () => {
     return (
-        <div className="faq">
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
@@ -12,6 +14,7 @@ const FaqSection = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, cupiditate?
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
@@ -21,6 +24,7 @@ const FaqSection = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, cupiditate?
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Different Payment Methods</h4>
@@ -30,6 +34,7 @@ const FaqSection = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, cupiditate?
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What Products do you offer?</h4>
@@ -39,9 +44,38 @@ const FaqSection = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, cupiditate?
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
     )
 }
+
+const Faq = styled(About)`
+    display: block;
+    span{
+        display: block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line{
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question{
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer{
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`;
+
 
 export default FaqSection;
--- capture/src/components/GlobalStyle.js
+++ capture/src/components/GlobalStyle.js
@@ -10,9 +10,10 @@ const GlobalStyle = createGlobalStyle`
 
 body{
     background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
 }
 
-button{
+button{    
     font-weight: bold;
     font-size: 1.1rem;
     cursor: pointer;
@@ -21,6 +22,7 @@ button{
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+    font-family: 'Inter', sans-serif;
     &:hover{
         background-color: #23d997;
         color: white;
@@ -39,6 +41,7 @@ h3{
 
 h4{
     font-weight: bold;
+    font-size: 2rem;
 }
 
 a{
