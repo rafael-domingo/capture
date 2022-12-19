c4f7d32e1f913e073fc6f02ba7d50d857256f55c
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2022-12-18 20:56:40 -0600

    Set up React Router

--- capture/src/App.js
+++ capture/src/App.js
@@ -2,12 +2,27 @@
 import GlobalStyle from "./components/GlobalStyle";
 // Import Pages 
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from './components/Nav';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">      
+    <div className="App">            
       <GlobalStyle />
-      <AboutUs />
+      <Nav />      
+      <Routes>      
+        <Route path="/" exact element={<AboutUs />}/>
+        <Route path="/work" exact element={<OurWork />} />
+        <Route path="/contact" exact element={<ContactUs />}/>
+      </Routes>
+      
+      
+      
+      
+      
     </div>
   );
 }
